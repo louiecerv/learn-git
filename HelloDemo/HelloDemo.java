a677bfdc6c45008554a5cbd6048ef91ebf04ff6b
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2015-08-03 14:47:49 +0800

    Added Hello Demo
    New files in Hello Demo

---
+++ HelloDemo/HelloDemo.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class HelloDemo here.
+ * 
+ * @author Louie F. Cervantes
+ * @version mm/dd/2015
+ */
+public class HelloDemo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Demo!");
+    }
+}
